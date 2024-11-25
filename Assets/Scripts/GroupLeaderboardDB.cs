df90f6f3a0cd79a784f81bda8c3613a607f9fe50
clydie-juls <clyde_marindo@dlsu.edu.ph>
2024-11-25 15:46:31 +0800

    feat: add db feature on LeaderboardDb

--- Assets/Scripts/GroupLeaderboardDB.cs
+++ Assets/Scripts/GroupLeaderboardDB.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a2ac71505980ced287ccecda404e4d3206a35e54a524ce2983250e3a8ae344f
-size 709
+oid sha256:b5dcc1ab8d4279c939aa22de1619c0ba0b960eff0f0a9ec1fe00a8f759095787
+size 1549
--- Assets/Scripts/PlayerDB.cs
+++ Assets/Scripts/PlayerDB.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2f38c11af518332144a37be3848aa745cc973dcc4c8861b8128c29f8f5db231
-size 1750
+oid sha256:961cd263948f6a0f7fba84f39a8707d8beae24840882e806efb1373de0bd9854
+size 1712
